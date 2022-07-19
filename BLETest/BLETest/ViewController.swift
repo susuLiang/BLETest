f88f4b0fc37f36c8599c78b9ff77c36ea9d8f755
Shu Wei <sophie790709@gmail.com>
2022-07-19 11:50:41 +0800

    project init

---
+++ BLETest/BLETest/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  BLETest
+//
+//  Created by Susu on 2022/7/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
+
