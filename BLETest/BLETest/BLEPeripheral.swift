99e9e4ef705479c241fb00dc014cfab3b9e96235
Shu Wei <sophie790709@gmail.com>
2022-07-20 12:52:49 +0800

    [add] set peripheral IDs

---
+++ BLETest/BLETest/BLEPeripheral.swift
@@ -0,0 +1,15 @@
+//
+//  BLEPeripheral.swift
+//  BLETest
+//
+//  Created by Susu on 2022/7/19.
+//
+
+import Foundation
+import CoreBluetooth
+
+class BLEPeripheral {
+    
+    public static let serviceUUID = CBUUID(string: "1fee6acf-a826-4e37-9635-4d8a01642c5d")
+    public static let batteryUUID = CBUUID(string: "7691b78a-9015-4367-9b95-fc631c412cc6")
+}
